887c682a3e5b23b28b0a65f01aa3e9c9c9ce47bd
T-Shikai <tatsuroshikai@gmail.com>
2021-01-01 00:44:21 +0000

    [finish] 3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,21 @@ class BlogsController < ApplicationController
     redirect_to blogs_path(blog.id)  end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blogs_path(blog)
+  end 
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end 
+
 
   private
   def blog_params
